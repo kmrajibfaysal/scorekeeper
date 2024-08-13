dd2c809b85bccb4a27ffddbbaee9d098422b5b7e
kmrajibfaysal <kmrajibfaysal@gmail.com>
2024-08-14 04:04:29 +0600

    added option selection feature for the play

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const p2_display = document.querySelector('#p2_score');
 const p1_button = document.querySelector('#p1_button');
 const p2_button = document.querySelector('#p2_button');
 const reset_button = document.querySelector('#reset_button');
+const play_to = document.querySelector('#play_to');
 
 let p1_score = 0;
 let p2_score = 0;
-let winning_score = 5;
+let winning_score = 3;
 let isGameOver = false;
+
+// playing option selection code!
+play_to.addEventListener('change', () => {
+  winning_score = parseInt(play_to.value);
+  reset();
+});
 
 // player 1 button evening listener and functions
 p1_button.addEventListener('click', () => {
@@ -34,13 +41,16 @@ p2_button.addEventListener('click', () => {
   }
 });
 
-//   reset button
+//   reset function
 
-reset_button.addEventListener('click', () => {
+function reset() {
   isGameOver = false;
   p1_score = 0;
   p2_score = 0;
   heading.textContent = 'Play ongoing!';
   p1_display.textContent = p1_score;
   p2_display.textContent = p2_score;
-});
+  winning_score = parseInt(play_to.value);
+}
+
+reset_button.addEventListener('click', reset);
